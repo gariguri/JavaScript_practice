123cded2448d4f4f1124e68316e490d81ba27180
gaeon <rkdjs1104@ajou.ac.kr>
2024-05-03 08:16:30 +0900

    ✨feat: while 반복문 연습문제

--- while문/practice01.js
+++ while문/practice01.js
@@ -2,3 +2,12 @@ let number=parseInt(prompt("알림 내용"));
 while(number<=100){
     number=parseInt(prompt("알림 내용"));
 }
+
+
+
+while (true){
+    num=parseInt(prompt("100보다 큰 수를 입력해주세요"));
+    if(num>100){
+        break;
+    }
+}
--- while문/practice02.js
+++ while문/practice02.js
@@ -6,9 +6,9 @@ function limitCalls(func, n) { // 여기에 코드를 작성하세요.
             func();
             num++;
         }
-        if (num===n){
-            return 0;
-        }
+        // if (num===n){
+        //     return 0;
+        // }
     };
 }
 const limitedHello = limitCalls(() => console.log("Hello!"), 2); 
