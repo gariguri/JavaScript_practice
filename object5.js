d57f8473c2f44c2578302f092d4ace40d999da11
gaeon <rkdjs1104@ajou.ac.kr>
2024-05-02 13:55:20 +0900

    ✨feat: object5 객체 내 이름 추가

--- object5.js
+++ object5.js
@@ -1,3 +1,4 @@
+const myName="김가언";
 let options={
     title: "Menu",
     width:100,
@@ -11,6 +12,7 @@ let options2={
     ...options, //{ title: 'Menu', width: 100, height: 200, k1: 'v1', k2: 'v2' }
     width: 500, //키값이 중복되면 덮어 씌움.
     title: "newTitle",
+    myName //변수 이름 자체가 키값으로 들어간다.
 }
 console.log(options2);
 
