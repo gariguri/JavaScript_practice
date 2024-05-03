b82399203931b5ac8d9d28458f9b59661c6e9719
gaeon <rkdjs1104@ajou.ac.kr>
2024-05-03 11:42:45 +0900

    ✨feat: promise 심화학습

---
+++ promise-all.js
@@ -0,0 +1,27 @@
+const delay=(ms) => new Promise(resolve=> setTimeout(resolve, ms)); //promise 객체, 비동기 코드 흐름을 기다렸다가 실행한다.
+async function fastFunction(data){
+    await delay(1000); //resolve되기를 기다리는 await 키워드! resolve의 인자로 넣어준 값이 return 되는 것 
+    const result=data*2;
+    console.log("Fast Function done", result);
+    return result;
+}
+
+
+async function slowFunction(data){
+    await delay(3000);
+    const result=data+10;
+    console.log('slow function done',result);
+    return result;
+}
+
+function runTask(){
+    Promise.all(
+        [fastFunction(5), slowFunction(10)]
+    ).then(([result1, result2])=>{
+        console.log('작업종료');
+        console.log(result1);
+        console.log(result2);
+    })
+
+}
+runTask();
---
+++ promise-race.js
@@ -0,0 +1,24 @@
+const delay=(ms) => new Promise(resolve=> setTimeout(resolve, ms)); //promise 객체, 비동기 코드 흐름을 기다렸다가 실행한다.
+async function fastFunction(data){
+    await delay(1000); //resolve되기를 기다리는 await 키워드! resolve의 인자로 넣어준 값이 return 되는 것 
+    const result=data*2;
+    console.log("Fast Function done", result);
+    return result;
+}
+
+
+async function slowFunction(data){
+    await delay(3000);
+    const result=data+10;
+    console.log('slow function done',result);
+    return result;
+}
+
+function runTask(){
+    Promise.race([fastFunction(5), slowFunction(10)]).then(data=>{
+        console.log("작업완료");
+        console.log(data);
+    })
+
+}
+runTask();
